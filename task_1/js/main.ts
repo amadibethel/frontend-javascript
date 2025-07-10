a4e243f13e5ebb82f8026075d15b9d4a84e1e388
Bethel Amadi <amadibethel@icloud.com>
2025-07-10 17:08:26 +0200

    Add updated files for TypeScript Project

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,18 +1,54 @@
+// Task 1: Teacher Interface
+
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // allows additional properties like contract
+  [key: string]: any; // allows additional properties
 }
+
+// Example usage of Teacher
 
 const teacher3: Teacher = {
   firstName: 'John',
-  fullTimeEmployee: false,
   lastName: 'Doe',
+  fullTimeEmployee: false,
   location: 'London',
   contract: false,
 };
 
 console.log(teacher3);
+
+// Task 2: Directors Interface
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage of Directors
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// Task 3: printTeacher function + interface
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+// Example usage of printTeacher
+
+console.log(printTeacher("John", "Doe")); // J. Doe
